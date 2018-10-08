5b3fcf84b434b7ad3924eb667245df99dfacdc8f
Andrew Gallin <andrewgallin@gmail.com>
2018-10-07 21:20:12 -0400

    added axes and started on data processing

--- src/App.js
+++ src/App.js
@@ -2,15 +2,40 @@ import React, { Component } from 'react';
 import './App.css';
 import BarChart from './bar'
 
+
 class App extends Component {
+
+  process(data) {
+    let dataArr = []
+    data.forEach((val) => {
+      let date = val[0].split('-')
+      switch(date[1]){
+        case '01':
+          dataArr.push([date[0], val[1], 'Q1'])
+        case '04':
+          dataArr.push([date[0], val[1], 'Q2'])
+        case '07':
+          dataArr.push([date[0], val[1], 'Q3'])
+        case '10':
+          dataArr.push([date[0], val[1], 'Q4'])
+        default:
+          console.log('ERROR')
+      }})
+    }
   render() {
+    var Httpreq = new XMLHttpRequest(); // a new request
+    Httpreq.open("GET",'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json',false);
+    Httpreq.send(null);
+
+    var json_obj = JSON.parse(Httpreq.responseText)
+    console.log(json_obj.data)
     return (
       <div className="App">
         <div className='App-header'>
           <h2>d3ia dashboard</h2>
         </div>
         <div>
-          <BarChart data={[5,10,1,3]} size={[500,500]} />
+          <BarChart data={json_obj.data} size={[500,500]} />
         </div>
       </div>
     );
--- src/bar.js
+++ src/bar.js
@@ -4,6 +4,7 @@ import { scaleLinear } from 'd3-scale';
 import { max } from 'd3-array';
 import { select } from 'd3-selection';
 import { axisBottom } from 'd3-axis';
+import { axisLeft } from 'd3-axis';
 
 class BarChart extends Component {
    constructor(props){
@@ -18,14 +19,16 @@ class BarChart extends Component {
    }
    createBarChart() {
       const node = this.node
+      const padding = 50
       const dataMax = max(this.props.data)
       const xScale = scaleLinear()
-                     .domain([0, dataMax])
-                     .range([0, this.props.size[0]]);
+                     .domain([0, max(this.props.data, (d) => d[0])])
+                     .range([padding, this.props.size[0]]);
       const yScale = scaleLinear()
-         .domain([0, dataMax])
-         .range([0, this.props.size[1]])
+         .domain([0, max(this.props.data, (d) => d[1])])
+         .range([this.props.size[1], padding])
       const xAxis = axisBottom(xScale);
+      const yAxis = axisLeft(yScale);
 
 
    select(node)
@@ -40,16 +43,19 @@ class BarChart extends Component {
       .exit()
       .remove()
 
-   select(node).append("g").attr("transform", "translate(0, 10)").attr("id", "x-axis")
+   select(node).append("g").attr("transform", "translate(0, 500)").attr("id", "x-axis")
    .call(xAxis);
    
+   select(node).append("g").attr("transform", "translate(50, 0)").attr("id", "y-axis")
+   .call(yAxis);
+
    select(node)
       .selectAll('rect')
       .data(this.props.data)
       .style('fill', '#fe9922')
-      .attr('x', (d,i) => i * 25)
-      .attr('y', d => this.props.size[1] - yScale(d))
-      .attr('height', d => yScale(d))
+      .attr('x', (d,i) => i * 5 + padding)
+      .attr('y', d => this.props.size[1] - yScale(d[1]))
+      .attr('height', d => yScale(d[1]))
       .attr('width', 25)
    }
 render() {
@@ -57,7 +63,7 @@ render() {
       <div>
          <h1 id='title'> d3ia dashboard</h1>
          <svg ref={node => this.node = node}
-         width={500} height={500}>
+         width={550} height={550}>
          </svg>
       </div>
       )
