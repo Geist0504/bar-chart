1f315d1ee0bf9b242ea01a51c25c72d76c3bd7bb
Andrew Gallin <andrewgallin@gmail.com>
2018-10-09 07:45:36 -0400

    Updates

---
+++ precache-manifest.df8cbac44e80134728bbd079ae0abe38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "913fa3de31966802f8f5",
+    "url": "/bar-chart/static/js/runtime~main.913fa3de.js"
+  },
+  {
+    "revision": "5b8271faba7069d1c56e",
+    "url": "/bar-chart/static/js/main.5b8271fa.chunk.js"
+  },
+  {
+    "revision": "3b695678c4813add6d13",
+    "url": "/bar-chart/static/js/1.3b695678.chunk.js"
+  },
+  {
+    "revision": "5b8271faba7069d1c56e",
+    "url": "/bar-chart/static/css/main.45391ea5.chunk.css"
+  },
+  {
+    "revision": "faad22026188339a9b68ec732dcd6c95",
+    "url": "/bar-chart/index.html"
+  }
+];
